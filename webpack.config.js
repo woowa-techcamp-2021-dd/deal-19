b1c6c761183fabd483e5f17685671bde577b1c0d
jjunyjjuny <jjuny546@gmail.com>
2021-07-13 20:48:29 +0900

    Init: express 세팅

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+import webpack from "webpack";
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
@@ -6,10 +7,12 @@ const PORT = process.env.PORT || 8080;
 
 export default {
   mode: "development",
-  entry: "./src/index.js",
+  target: "web",
+  entry: ["./public/javascripts/main.js", "webpack-hot-middleware/client"],
   output: {
     filename: "[name].js",
     path: path.join(__dirname, "dist"),
+    publicPath: "/",
   },
   devtool: "source-map",
   module: {
@@ -19,14 +22,27 @@ export default {
         use: ["css-loader", "sass-loader"],
         exclude: /node_modules/,
       },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: "public/index.html",
     }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
   devServer: {
     port: PORT,
+    hot: true,
   },
 };
