d122138253b3fe7c0b9dcdeba805b46a3bd1acc2
jjunyjjuny <jjuny546@gmail.com>
2021-07-13 21:26:33 +0900

    Init: eslint 설정

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import sassMiddleware from "node-sass-middleware";
-import webpackDevMiddleware from "webpack-dev-middleware";
-import webpackHotMiddleware from "webpack-hot-middleware";
-import webpack from "webpack";
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import sassMiddleware from 'node-sass-middleware';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpack from 'webpack';
 
-import indexRouter from "./routes/index.js";
+import indexRouter from './routes/index.js';
 
-import config from "./webpack.config.js";
+import config from './webpack.config.js';
 const compiler = webpack(config);
 
 const __dirname = path.resolve();
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   sassMiddleware({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
     indentedSyntax: false, // true = .sass and false = .scss
-    sourceMap: true,
+    sourceMap: true
   })
 );
 
 app.use(
   webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
+    publicPath: config.output.publicPath
   })
 );
 app.use(
   webpackHotMiddleware(compiler, {
-    heartbeat: 2 * 1000,
+    heartbeat: 2 * 1000
   })
 );
 
-// app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
+app.use('/', indexRouter);
 
 export default app;
--- bin/www.js
+++ bin/www.js
@@ -2,23 +2,23 @@
  * Module dependencies.
  */
 
-import app from "../app.js";
-import DEBUG from "debug";
-import http from "http";
-var debug = DEBUG("server:server");
+import app from '../app.js';
+import DEBUG from 'debug';
+import http from 'http';
+const debug = DEBUG('server:server');
 
 /**
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
 
 /**
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+const server = http.createServer(app);
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -28,15 +28,15 @@ server.listen(port, () => {
   console.log(`Express Server ${port}`);
   console.log(`http://localhost:${port}`);
 });
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 /**
  * Normalize a port into a number, string, or false.
  */
 
-function normalizePort(val) {
-  var port = parseInt(val, 10);
+function normalizePort (val) {
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -55,23 +55,21 @@ function normalizePort(val) {
  * Event listener for HTTP server "error" event.
  */
 
-function onError(error) {
-  if (error.syscall !== "listen") {
+function onError (error) {
+  if (error.syscall !== 'listen') {
     throw error;
   }
 
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
       process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
       process.exit(1);
-      break;
     default:
       throw error;
   }
@@ -81,8 +79,8 @@ function onError(error) {
  * Event listener for HTTP server "listening" event.
  */
 
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
+function onListening () {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  debug('Listening on ' + bind);
 }
