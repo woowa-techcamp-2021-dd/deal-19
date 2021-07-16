99b0c42de7fff4f1a78edad69d9c1facda6b419a
edegiil <electropark917@gmail.com>
2021-07-16 21:16:27 +0900

    Style: 드롭다운 추가

---
+++ public/components/dropdown.js
@@ -0,0 +1,45 @@
+import Component from '../core/component.js';
+import _ from '../utils/dom.js';
+
+import './dropdown.scss';
+
+export default class Dropdown extends Component {
+  getTemplate () {
+    const { buttonList } = this.props;
+
+    return `
+      <div class="dropdown__container">
+        ${
+          buttonList.map((button, i) => {
+            const { label, color } = button;
+
+            return (`
+              <button
+                id="dropdown-button-${i}"
+                ${color ? `style="color: ${color}"` : ''}
+              >
+                ${label}
+              </button>
+            `);
+          }).join('')
+        }
+      </div>
+    `;
+  }
+
+  setEventListener () {
+    const { buttonList } = this.props;
+
+    this.addEventListener('mouseenter', `#${this.$target.id}`, (e) => {
+      _.$('.dropdown__container').classList.add('open');
+    });
+
+    this.addEventListener('mouseleave', `#${this.$target.id}`, (e) => {
+      _.$('.dropdown__container').classList.remove('open');
+    });
+
+    buttonList.forEach(({ action }, i) => {
+      this.addEventListener('click', `#dropdown-button-${i}`, action);
+    });
+  }
+}
