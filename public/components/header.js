78ab7cbe02490453b95a76ddef83b347041d976a
jjunyjjuny <jjunyjjuny@gmail.com>
2021-07-17 22:00:43 +0900

    update: h헤더 중복시 클래스 중복을 막기 위해 css class네임에 {type} 추가

--- public/components/header.js
+++ public/components/header.js
@@ -1,16 +1,16 @@
 import Component from '../core/component.js';
 import _ from '../utils/dom.js';
 import './header.scss';
+// import DropDown from '../components/dropdown.js';
 
 export default class Header extends Component {
   getTemplate () {
     const { type, title } = this.props;
-
     return `
       <div class="header ${type}">
-        <div class="header__left-box">${renderHeaderLeft(type)}</div>
-        <div class="header__title">${title}</div>
-        <div class="header__right-box">
+        <div class="${type} header__left-box">${renderHeaderLeft(type)}</div>
+        <div class="${type} header__title">${type !== 'mian' ? title : ''}</div>
+        <div class="${type} header__right-box">
           ${renderHeaderRight(type)}
         </div>
       </div>
@@ -20,7 +20,7 @@ export default class Header extends Component {
   setEventListener () {
     const { type, handlerClickRightIcon, hideSubPageBySlide } = this.props;
 
-    const $left = _.$('.header__left-box');
+    const $left = _.$(`.header__left-box.${type}`);
     if (type && !['main', 'write', 'detail', 'chat'].includes(type)) {
       throw new Error('정확한 타입 지정 & 아예 쓰지 말 것');
     }
@@ -37,8 +37,10 @@ export default class Header extends Component {
     }
 
     $left.addEventListener('click', () => {
+      console.log(1);
       if (hideSubPageBySlide) {
         hideSubPageBySlide();
+        console.log('슬라이더 닫기');
         return;
       }
       moveToPrevPage();
@@ -47,22 +49,23 @@ export default class Header extends Component {
 
   setMainHeaderHandler () {
     const { showLocation } = this;
-    const { showSubPageBySlide } = this.props;
+    const { showSubPageBySlide, type } = this.props;
 
-    _.$('.header__left-box').addEventListener('click', () => {
+    _.$(`.header__left-box.${type}`).addEventListener('click', () => {
       showSubPageBySlide('category');
     });
-    _.$('.header__title').addEventListener('click', showLocation);
-    _.$('.header__my-account').addEventListener('click', () => {
+    _.$(`.header__title.${type}`).addEventListener('click', showLocation);
+    _.$(`.header__my-account.${type}`).addEventListener('click', () => {
       showSubPageBySlide('myAccount');
     });
-    _.$('.header__menu').addEventListener('click', () => {
+    _.$(`.header__menu.${type}`).addEventListener('click', () => {
       showSubPageBySlide('myMenu');
     });
   }
 
   setRightIconHandler (handler) {
-    const $rightIcon = _.$('.header__right-box > div');
+    const { type } = this.props;
+    const $rightIcon = _.$(`.header__right-box.${type} > div`);
     $rightIcon.addEventListener('click', handler);
   }
 
@@ -87,11 +90,9 @@ const renderHeaderRight = (type) => {
 
   switch (type) {
     case 'main':
-      return '<div class="wmi-user header__my-account"></div><div class="wmi-menu header__menu"></div>';
+      return `<div class="wmi-user header__my-account ${type}"></div><div class="wmi-menu header__menu ${type}"></div>`;
     case 'write':
       return '<div class="wmi-check"></div>';
-    case 'detail':
-      return '<div class="wmi-more-vertical"></div>';
     case 'chat':
       return '<div class="wmi-log-out"></div>';
     default :
