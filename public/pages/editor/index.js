5648da21a76707507d9a18ddff4c40afed3ccdc7
jjunyjjuny <jjunyjjuny@gmail.com>
2021-07-20 01:10:00 +0900

    Style: editor 마크업 복구 / 개선

--- public/pages/editor/index.js
+++ public/pages/editor/index.js
@@ -1,3 +1,44 @@
 import './style.scss';
+import Component from '../../core/component.js';
+import _ from '../../utils/dom.js';
+import Header from '../../components/header.js';
+import EditorForm from './editorForm/index.js';
 
-console.log('editor');
+const $root = document.querySelector('#root');
+
+class App extends Component {
+  initState () {
+    this.state = { location: '역삼동' };
+  }
+
+  getTemplate () {
+    return `
+        <div id="main__header"></div>
+        <div id="main__contents"></div>
+    `;
+  }
+
+  mountChildren () {
+    const { requestAddPosting } = this;
+    const { location } = this.state;
+
+    const $header = _.$('#main__header');
+    const $contens = _.$('#main__contents');
+
+    new Header($header, {
+      type: 'write',
+      title: '글쓰기',
+      handlerClickRightIcon: requestAddPosting.bind(this)
+    });
+
+    new EditorForm($contens, { location });
+  }
+
+  requestAddPosting () {
+    console.log('글쓰기 요청');
+  }
+
+  setEventListener () {}
+}
+
+new App($root);
