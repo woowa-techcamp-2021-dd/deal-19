84b5eb3adab0892251d118fb5ce0c2b72e01f8f9
jjunyjjuny <jjunyjjuny@gmail.com>
2021-07-19 17:21:37 +0900

    update: 슬라이더 내부 헤더  생성 책임을 컨텐츠 요소에게 위임하는 방식으로 변경

--- public/components/slider.js
+++ public/components/slider.js
@@ -1,6 +1,5 @@
 import Component from '../core/component.js';
 import _ from '../utils/dom.js';
-import Header from './header.js';
 import './slider.scss';
 
 export default class Slider extends Component {
@@ -9,24 +8,14 @@ export default class Slider extends Component {
 
     return `
       <div class="slider__container ${sliderState}">
-        <header class="slider__header"></header>
-        <div class="slider__contents"></div>
       </div>
     `;
   }
 
   mountChildren () {
-    const { closeSlider, Constructor, title } = this.props;
+    const { Constructor, innerProps } = this.props;
 
-    const $header = _.$('.slider__header');
-    const $container = _.$('.slider__contents');
-
-    new Header($header, { title, closeSlider });
-
-    new Constructor($container, {});
-  }
-
-  setEventListener () {
-
+    const $container = _.$('.slider__container');
+    new Constructor($container, innerProps);
   }
 }
