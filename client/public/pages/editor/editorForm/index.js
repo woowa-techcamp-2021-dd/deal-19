71d0dcf107befa97798b0195cf8591e59338d0b7
jjunyjjuny <jjunyjjuny@gmail.com>
2021-07-20 18:39:25 +0900

    Style: 글쓰기 화면 마크업 변경

--- client/public/pages/editor/editorForm/index.js
+++ client/public/pages/editor/editorForm/index.js
@@ -3,83 +3,73 @@ import _ from '../../../utils/dom.js';
 import './style.scss';
 
 export default class EditorForm extends Component {
-  initState () {
-    this.state = {
-      imageList: ['https://i.guim.co.uk/img/media/8a13052d4db7dcd508af948e5db7b04598e03190/0_294_5616_3370/master/5616.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=bcaa4eed2c1e6dab61c41a61e41433d9', 'https://i.guim.co.uk/img/media/8a13052d4db7dcd508af948e5db7b04598e03190/0_294_5616_3370/master/5616.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=bcaa4eed2c1e6dab61c41a61e41433d9', 'https://i.guim.co.uk/img/media/8a13052d4db7dcd508af948e5db7b04598e03190/0_294_5616_3370/master/5616.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=bcaa4eed2c1e6dab61c41a61e41433d9'],
-      name: '빈티지 롤러 스케이트',
-      category: '',
-      categoryList: ['여성패션/잡화', '기타 중고물품', '가구/인테리어'],
-      price: 169000,
-      contents: '어린시절 추억의 향수를 어쩌구 저쩌구'
-    };
-  }
-
   getTemplate () {
-    const { imageList, name, category, categoryList, price, contents } = this.state;
-    const { location } = this.props;
+    const { imageList, name, category, categoryList, price, contents, location } = this.props;
     return `
-      <form id="editor__form" action="http://localhost:3000/api/items" method="post"> 
-        <div class="write__image-list content-box">
-          <input name="images" id="input-image" type="file" accept="img/*"  multiple />
-          <div class="button-add-image">추가</div>
-          <div class="carousel">
+      <div class="write__image-list content-box">
+        <input class="input-area" id="input-image" type="file" accept="img/*"  multiple />
+        <div class="image-box button-add-image">
+          <div class="wmi-image"></div>
+          <div>추가</div>
+        </div>
+        <div class="carousel">
           ${imageList
             .map(
               (src) =>
-                `<figure class="image-box image-added"><img src=${src} /></figure>`
+                `<div class="image-added image-box">
+                  <figure>
+                    <img src=${src} />
+                  </figure>
+                  <div class="wmi-close button-delete-image"></div>
+                </div>`
             )
-            .join('')}
+            .join("")}
         </div>
-        </div>
-        <div class="write__info content-box">
+      </div>
+      <div class="write__info content-box">
         <div>
-          <input type="text" name="name" >${name}</h2>
-        </div>
-        <div class="category">${
-          category === '' ? '(필수) 카테고리를 선택해주세요.' : category
-        }</div>
-        <div class="carousel">
-        ${categoryList
-          .map(
-            (item, i) =>
-              `<div class="category-item">
-                <input type="radio" id="category-${i}" name="category" value="${item}">
-                <label for="category-${i}">${item}</label>
-              </div>`
-          )
-          .join('')}</div>
-        </div>
-        <div class="write__price content-box">
-          <input type="number" value="${price}" name="price"/>
-        </div>
-        <div class="write__contents content-box">
-          <textarea>${contents}</textarea>
-        </div>
+        <input class="input-area" id="input-name" type="text" value="${name}" name="name" >
+      </div>
+      <div class="category">${
+        category.id === "" ? "(필수) 카테고리를 선택해주세요." : category.title
+      }</div>
+      <div class="carousel">
+      ${categoryList
+        .map(
+          ({ title, id }) =>
+            `<div class="category-item">
+            <input class="input-area" type="radio" id="category-${id}" name="category" value="${id}">
+            <label for="category-${id}">${title}</label>
+          </div>`
+        )
+        .join("")}</div>
+      </div>
+      <div class="write__price content-box">
+        <input id="input-price" class="input-area" type="text" name="price" value="${addCommaToPrice(
+        price
+      )}" />
+      </div>
+      <div class="write__contents content-box">
+        <textarea id="input-contents" name="contents" class="input-area">${contents}</textarea>
+      </div>
         <div class="write__footer" >
-          <input type="text" value="${location}" name="location"/>
-        </div>
-      </form>
-    `;
+      <div >${location}</div>
+    </div>`;
   }
 
   mountChildren () {
 
   }
 
-  requestAddPosting () {
-    console.log('글쓰기 요청');
-  }
-
   setEventListener () {
-    const { send } = this.props;
-    const $form = _.$('form');
-    $form.addEventListener('submit', (e) => {
-      console.log(e);
-      // e.preventDefault();
-    });
-    this.addEventListener('click', '.button-add-image', () => {
-      const $inputImage = _.$('#input-image');
-      $inputImage.click();
+    const { onChangeInput } = this.props;
+    this.addEventListener('change', '.input-area', ({target}) => {
+      const {name, value} = target;
+      onChangeInput({[name]: value})
     });
   }
+}
+
+const addCommaToPrice = (price) => {
+  return price.toLocaleString()
 }
