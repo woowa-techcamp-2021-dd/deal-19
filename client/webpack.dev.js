f14d0481d75220f395b7a160ca8cc22effb0caae
edegiil <electropark917@gmail.com>
2021-07-20 14:20:37 +0900

    Update: api 클라이언트 분리

---
+++ client/webpack.dev.js
@@ -0,0 +1,20 @@
+import webpack from 'webpack';
+import { merge } from 'webpack-merge';
+
+import common from './webpack.common.js';
+
+const PORT = process.env.PORT || 9000;
+
+export default merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: 'dist',
+    port: PORT,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ]
+});
