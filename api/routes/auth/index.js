790f5ab2181f327dfcd29e28b9983ad685cd76b4
edegiil <electropark917@gmail.com>
2021-07-21 22:34:05 +0900

    Update: head 라우트 추가

--- api/routes/auth/index.js
+++ api/routes/auth/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
+import head from './head.js';
 import signin from './signin/index.js';
 import signup from './signup/index.js';
 import towns from './towns/index.js';
 
 const router = express.Router();
 
+router.use('/', head);
 router.use('/signin', signin);
 router.use('/signup', signup);
 router.use('/towns', towns);
