433ef8b7ff3651bcbca9d84af06fd818a905f90d
edegiil <electropark917@gmail.com>
2021-07-23 02:47:43 +0900

    Delete: 채팅 기능 삭제

--- client/routes/index.js
+++ client/routes/index.js
@@ -4,16 +4,12 @@ import main from './main/index.js';
 import productDetail from './productDetail/index.js';
 import location from './location/index.js';
 import editor from './editor/index.js';
-import chatList from './chatList/index.js';
-import chatDetail from './chatDetail/index.js';
 
 const router = express.Router();
 
 router.use('/', main);
 router.use('/productDetail', productDetail);
 router.use('/editor', editor);
-router.use('/chatList', chatList);
-router.use('/chatDetail', chatDetail);
 router.use('/location', location);
 
 export default router;
