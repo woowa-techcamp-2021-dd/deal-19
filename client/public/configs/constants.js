be58c81e6580dcf153dfe34b3cb0073c8bbb8b95
edegiil <electropark917@gmail.com>
2021-07-23 02:46:21 +0900

    Update: 기타 파일 추가

--- client/public/configs/constants.js
+++ client/public/configs/constants.js
@@ -1,59 +1,93 @@
 export const categoryList = [
   {
+    title: '전체보기',
+    id: '',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/global.png'
+  },
+  {
     title: '디지털기기',
-    id: 'digital'
+    id: 'digital',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/polaroid.png'
   },
   {
     title: '생활가전',
-    id: 'appliances'
+    id: 'appliances',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/polaroidcamera.png'
   },
   {
     title: '가구/인테리어',
-    id: 'furniture'
+    id: 'furniture',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/door.png'
   },
   {
     title: '게임/취미',
-    id: 'game'
+    id: 'game',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/gamecontroller.png'
   },
   {
     title: '생활/가공식품',
-    id: 'life'
+    id: 'life',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/1176/dine_o_matic/128/fast_food.png'
   },
   {
     title: '스포츠/레저',
-    id: 'sport'
+    id: 'sport',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/biker.png'
   },
   {
     title: '여성패션/잡화',
-    id: 'womenFashion'
+    id: 'womenFashion',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/fashion.png'
   },
   {
     title: '남성패션/잡화',
-    id: 'menFashion'
+    id: 'menFashion',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/star.png'
   },
   {
     title: '유아동',
-    id: 'kids'
+    id: 'kids',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2219/dot_pictograms/128/baby.png'
   },
   {
     title: '뷰티/미용',
-    id: 'beauty'
+    id: 'beauty',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/scissors.png'
   },
   {
     title: '반려동물',
-    id: 'pet'
+    id: 'pet',
+    isActive: false,
+    src: 'https://cdn.datta.store/auxapi/files/image/cute-cat-kitty-head-kawaii.png'
   },
   {
     title: '도서/티켓/음반',
-    id: 'book'
+    id: 'book',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/booklet.png'
   },
   {
     title: '식물',
-    id: 'plant'
+    id: 'plant',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/flower.png'
   },
   {
     title: '기타 중고물품',
-    id: 'etc'
+    id: 'etc',
+    isActive: false,
+    src: 'https://findicons.com/files/icons/2787/beautiful_flat_icons/128/bolt.png'
   }
 ];
 
@@ -63,3 +97,5 @@ export const PRODUCT_STATUS = {
   ONSALE: 'onslae'
 };
 
+export const SELL_TAB = 'sell';
+export const LIKE_TAB = 'liked';
